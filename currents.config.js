b39e6a6e63ca836b996d577604f07076816d900a
Man Huynh <Man.Huynh@noria.no>
2025-05-07 14:28:38 +0700

    Update cloudSerivceUrl value

--- currents.config.js
+++ currents.config.js
@@ -2,5 +2,5 @@
 module.exports = {
     projectId: "sorry-cypress", // the projectId, can be any values for sorry-cypress users
     recordKey: "noria-01-04-2025", // the record key, can be any value for sorry-cypress users
-    cloudServiceUrl: "http://localhost:1234",   // Sorry Cypress users - set the director service URL
+    cloudServiceUrl: "https://positive-quintina-stephenhuynh-d5ec3181.koyeb.app",   // Sorry Cypress users - set the director service URL
 };
